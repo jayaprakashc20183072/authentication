c78754fb38adf8bb44d57c526cb93e3e18883425
Jayaprakash Reddy <chembeti.jayaprakash-reddy@capgemini.com>
2023-05-20 10:47:43 +0530

    hashing and salting

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 //jshint esversion:6
 require("dotenv").config();
 const express = require("express");
+const md5 = require("md5");
 const ejs = require("ejs");
+const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 const app = express();
+const saltRounds = 10;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static("public"));
@@ -15,7 +18,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields:["password"]});
+
 const User = mongoose.model("User",userSchema);
 
 
@@ -29,26 +32,31 @@ app.get("/login",function(req,res){
     res.render("login");
 });
 app.post("/register",function(req,res){
-    const newUser = new User (
-        {
-            email: req.body.username,
-            password: req.body.password
-        }
-    );
-    newUser.save().then(()=>{
-        res.render("secrets");
-        console.log(newUser.password);
-    }).catch((err)=>{console.log(err)})
+    bcrypt.hash(req.body.password,saltRounds).then((hash)=>{
+        const newUser = new User (
+            {
+                email: req.body.username,
+                password: hash
+            }
+        );
+        newUser.save().then(()=>{
+            res.render("secrets");
+            
+        }).catch((err)=>{console.log(err)})
+    })
+   
 });
 app.post("/login",function(req,res){
     const username = req.body.username;
     const password = req.body.password;
     User.findOne({email: username}).then((user)=>{
         if(user){
-            if(user.password===password){
+           bcrypt.compare(password,user.password).then((result)=>{
+            if(result===true){
                 res.render("secrets");
-                console.log(user.password);
+               
             }
+           })
         }
     }).catch((err)=>{console.log(err)})
 });
