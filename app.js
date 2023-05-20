d97264e36f91fabe87286396f611a42a4e01012b
Jayaprakash Reddy <chembeti.jayaprakash-reddy@capgemini.com>
2023-05-20 20:30:50 +0530

    complete project

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs = require("ejs");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const bodyParser = require("body-parser");
+const FacebookStrategy = require('passport-facebook').Strategy;
 const mongoose = require("mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
@@ -25,7 +26,9 @@ mongoose.connect("mongodb://localhost:27017/userDB",{ useNewUrlParser: true});
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    facebookId: String,
+    secret: String
 });
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
@@ -57,6 +60,17 @@ passport.use(new GoogleStrategy({
     });
   }
 ));
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/",function(req,res){
     res.render("home");
@@ -69,6 +83,15 @@ app.get('/auth/google',
     // Successful authentication, redirect home.
     res.redirect('/secrets');
   });
+  app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 app.get("/register",function(req,res){
     res.render("register");
 });
@@ -77,12 +100,21 @@ app.get("/login",function(req,res){
 
 });
 app.get("/secrets",function(req,res){
+  User.find({secret: {$ne:null}}).then((users)=>
+  {
+    if(users){
+        res.render("secrets",{usersWithSecrets: users});
+    }
+  })
+});
+app.get("/submit",function(req,res){
     if(req.isAuthenticated){
-        res.render("secrets");
+        res.render("submit");
     }else{
         res.redirect("/login");
     }
 });
+
 app.get("/logout",function(req,res){
     req.logout(function(err){
         if(err){
@@ -122,7 +154,20 @@ app.post("/login",function(req,res){
             })
         }
     })
+
 });
+app.post("/submit",function(req,res){
+    const submittedSecret = req.body.secret;
+    
+    User.findById(req.user.id).then((user)=>
+    {
+        if(user){
+            user.secret = submittedSecret;
+            user.save().then(()=>{res.redirect("/secrets")})
+        }
+    }).catch((err)=>{console.log(err)});
+})
+
 app.listen(3000,function(){
     console.log("server started on port 3000");
 });
